b1444134827c0347d6a32b11b786d5fa483da175
Ajahid <jahidabdulmajeed@gmail.com>
2022-01-09 19:37:59 -0800

    Eclipse Class Code

---
+++ LastPracticeClassGitGHub/src/LastPracticeClass/LastClass.java
@@ -0,0 +1,14 @@
+package LastPracticeClass;
+
+public class LastClass {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Last Practice class in Eclipse using Git & Git Hub");
+
+		
+		
+	}
+
+}
